629223a513a09daaf92042f50d717e901396b540
etolh <huliang12@126.com>
2017-06-30 14:51:51 +0800

    2a: hello.py add user

--- hello.py
+++ hello.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World!</h1>'
 
+#url动态部分可以用<>修饰，再传入绑定的视图函数
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello %s!</h1>' % name
+
 if __name__ == '__main__':
     app.run(debug=True)
