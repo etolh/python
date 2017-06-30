026b35b06a9c5a2d5744bf47c3f6d82f2f9a3559
etolh <huliang12@126.com>
2017-06-30 16:32:51 +0800

    3c: error handler

--- hello.py
+++ hello.py
@@ -23,6 +23,15 @@ def user(name):
     # return '<h1>Hello %s!</h1>' % name
     return render_template('user.html',name=name)
 
+#错误处理页面:404:未知请求，500：内部错误
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     #app.run(debug=True)
     manager.run()
