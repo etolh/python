708cfa96088bcd51d204f6188538fdb707d6a74f
etolh <huliang12@126.com>
2017-07-01 15:07:28 +0800

    5a: mysql connect

--- hello.py
+++ hello.py
@@ -7,19 +7,59 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask import Flask, render_template, session, redirect, url_for, flash #4b,重定向
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+#config mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+'mysql://root:4854264@localhost:3306/web'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+#db refers to database
+db = SQLAlchemy(app)
+
 #defend csrf
 app.config['SECRET_KEY'] = 'hard to guess string'
+
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+#定义数据库表对应的模型:extends db.Model
+class Role(db.Model):
+    """docstring for Role"""
+    __tablename__ = 'role'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+
+    #one2many-relations:one,backref与__tablename__对应
+    users = db.relationship('User', backref='role')
+
+    ''' return string '''
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    """docstring for User"""
+    __tablename__ = 'User'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(50),unique=True)
+
+    #one2many-relations:many,ForeignKey与__tablename__对应
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+    ''' return string '''
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
 #定义表单类
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
 
 #app.route完成url映射的路由绑定：
 #当输入该url时，app程序对象会自动寻找对应的函数处理url请求
