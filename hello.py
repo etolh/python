70ac0c23b8449bee032c6a2793d90b6f4083152c
etolh <huliang12@126.com>
2017-07-01 16:13:09 +0800

    5d: database migration

--- hello.py
+++ hello.py
@@ -9,6 +9,7 @@ from wtforms.validators import Required
 from flask import Flask, render_template, session, redirect, url_for, flash #4b,重定向
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.script import Shell
+from flask.ext.migrate import Migrate,MigrateCommand
 
 app = Flask(__name__)
 
@@ -56,10 +57,15 @@ class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+
 def make_shell_context():
     return dict(app=app,db=db,User=User,Role=Role)
 
 manager.add_command("shell",Shell(make_context=make_shell_context))
+
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 #app.route完成url映射的路由绑定：
