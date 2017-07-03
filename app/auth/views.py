84331f52a54a72a3ab323bc2e252f3676a39b09b
etolh <huliang12@126.com>
2017-07-03 17:00:10 +0800

    8d: mail confirmation

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,6 +6,8 @@ from .forms import LoginForm, RegisterForm
 from .. import db
 from flask.ext.moment import Moment  #本地化时间
 from datetime import datetime
+from ..email import send_mail
+from flask.ext.login import current_user
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -38,7 +40,53 @@ def register():
         user = User(email=form.email.data,name=form.username.data,
             password=form.password.data)
         db.session.add(user)
-        flash('you can login.')
+        db.session.commit()
+
+        token = user.generate_confirmation_token()
+        send_mail(user.email, 'Confirm Your Account.','auth/email/confirm',user=user,token=token)
+        flash('A confirmation email has been sent to your email.\
+            you can login.')
+        
         return redirect(url_for('auth.login'))
 
     return render_template('auth/register.html',form=form,current_time=datetime.utcnow())
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    #验证用户，并激活
+    if current_user.confirmed:
+        #已激活
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint \
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        #已激活，返回主页
+        return redirect(url_for('main.index'))
+    #未激活，返回激活说明，重新发送激活邮件
+    return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_mail(current_user.email, 'Confirm Your Account.','auth/email/confirm',user=current_user,token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -9,6 +11,7 @@ def load_user(user_id):
 
 #定义数据库表对应的模型:extends db.Model
 class Role(db.Model):
+    
     __tablename__ = 'role'
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64),unique=True)
@@ -20,7 +23,9 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 class User(UserMixin,db.Model):
+    
     __tablename__ = 'User'
+    
     id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(64),unique=True,index=True)
     name = db.Column(db.String(50),unique=True,index=True)
@@ -29,6 +34,29 @@ class User(UserMixin,db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
     password_hash = db.Column(db.String(128))
+
+    #用户激活状态
+    confirmed = db.Column(db.Boolean,default=False)
+
+    #生成激活令牌
+    def generate_confirmation_token(self,expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'],expires_in = 3600)
+        return s.dumps({'confirm':self.id})
+
+    #激活
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'],expires_in = 3600)
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        
+        if data.get('confirm') != self.id:
+            return False
+
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     #password_hash列是根据password属性生成的
     @property
