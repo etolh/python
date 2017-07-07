5e2e5aba973dcbb3a9851611b0d3c99248867225
etolh <huliang12@126.com>
2017-07-07 10:16:10 +0800

    Chap11: generate fake users and posts

--- app/models.py
+++ app/models.py
@@ -73,7 +73,22 @@ class Post(db.Model):
     #多对一外键
     author_id = db.Column(db.Integer, db.ForeignKey('User.id'))
 
+    #生成虚拟列表
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
 
+        seed()
+        user_count = User.query.count()
+        for i in range(count):
+            u = User.query.offset(randint(0, user_count - 1)).first()
+            p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1, 3)),
+                    timestamp=forgery_py.date.date(True),
+                    author=u)
+            db.session.add(p)
+            db.session.commit()
+    
 
 class User(UserMixin,db.Model):
     
@@ -116,6 +131,28 @@ class User(UserMixin,db.Model):
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
 
+    #生成虚拟数据
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            u = User(email=forgery_py.internet.email_address(),
+                name=forgery_py.internet.user_name(True),
+                password=forgery_py.lorem_ipsum.word(),
+                confirmed=True,
+                realname=forgery_py.name.full_name(),
+                location=forgery_py.address.city(),
+                about_me=forgery_py.lorem_ipsum.sentence(),
+                member_since=forgery_py.date.date(True))
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except IntegrityError as e:
+                db.session.rollback()
 
     #生成邮箱对应的图像
     def gravatar(self, size=100, default='identicon', rating='g'):
