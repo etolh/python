9dd3e3a59ff8ef1ca0fa96d0bc14e4ec8d93822e
etolh <huliang12@126.com>
2017-07-07 15:31:17 +0800

    Chapter11: 为文章生成单独页面，提供文章连接

--- app/main/views.py
+++ app/main/views.py
@@ -99,3 +99,12 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html',form=form,user=user)
+
+
+#文章固定连接
+@main.route('/post/<int:id>')
+def post(id):
+    #由Post的id得到指定的Post
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
+
